ce05b96dbcd331a693411c3f4b8e852cfd024098
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-04 19:45:58 +0530

    added input filed and button

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,13 @@ function App() {
     <>
      <Navbar/>
      <div className='container mx-auto bg-slate-200 rounded-xl p-3 m-5'>
-      <div>
-        <h1 className='font-bold'>Your Todo's</h1>
+      <div className="addTodo">
+        <h2 className='font-bold text-xl'>Add a Todo</h2>
+        <input type="text"  placeholder='Enter Todos' className='bg-white p-2 rounded-2xl w-2xl mt-3 mb-3'/>
+        <button className='bg-slate-950 text-white  rounded-2xl  px-4 py-2 font-bold ml-3'>Add</button>
       </div>
+        <h1 className='font-bold text-xl'>Your Todo's</h1>
+      
      </div>
     </>
   )
