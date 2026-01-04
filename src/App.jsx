69277053d04c8a90165d09e384eb59adc9cd9d99
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-04 20:03:24 +0530

    added todo's theme and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-     <Navbar/>
-     <div className='container mx-auto bg-slate-200 rounded-xl p-3 m-5'>
-      <div className="addTodo">
-        <h2 className='font-bold text-xl'>Add a Todo</h2>
-        <input type="text"  placeholder='Enter Todos' className='bg-white p-2 rounded-2xl w-2xl mt-3 mb-3'/>
-        <button className='bg-slate-950 text-white  rounded-2xl  px-4 py-2 font-bold ml-3'>Add</button>
-      </div>
+      <Navbar />
+      <div className='container mx-auto bg-slate-200 rounded-xl p-3 m-5 min-h-[80vh]'>
+        <div className="addTodo">
+          <h2 className='font-bold text-xl'>Add a Todo</h2>
+          <input type="text" placeholder='Enter Todos' className='bg-white p-2 rounded-2xl w-2xl mt-3 mb-3' />
+          <button className='bg-slate-950 text-white  rounded-2xl  px-4 py-2 font-bold ml-3'>Add</button>
+        </div>
         <h1 className='font-bold text-xl'>Your Todo's</h1>
-      
-     </div>
+        <div className="todos ">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div className='buttons'>
+              <button onClick={handleEdit}><i className="fa-solid fa-pen-to-square ml-3"></i></button>
+              <button onClick={habdleDelete}><i className="fa-solid fa-trash ml-3 hover:text-red-600 "></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
